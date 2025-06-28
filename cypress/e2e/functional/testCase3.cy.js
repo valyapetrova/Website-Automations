176440f2192e0673d6d555ebbccbfb6a48768135
Valya Petrova <your.email@example.com>
2025-06-28 22:55:46 +0300

    fix: remove comments

--- cypress/e2e/functional/testCase3.cy.js
+++ cypress/e2e/functional/testCase3.cy.js
@@ -1,9 +1,6 @@
 it('3. Should display "visit" button on each card in the components section', () => {
   cy.visit('https://mind-wend-913065.framer.app/');
-  // cy.contains('Many types of components to customize');//to scrow
-
-  //'[data-framer-name="Card"]'
-  // Get all the cards and check each
+ 
   cy.get('.components-container')
     .find('[data-framer-name="Card"]')
     .each(($card) => {
